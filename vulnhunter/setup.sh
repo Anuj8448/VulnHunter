4d9e529751b2510371952b758a1d602af6f2916a
Anuj8448 <92680217+Anuj8448@users.noreply.github.com>
2024-08-02 19:20:44 +0530

    Add files via upload

---
+++ vulnhunter/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install golismero from the external source
+echo "Installing golismero..."
+apt-get install python2.7 python2.7-dev python-pip python-docutils git perl nmap sslscan
+cd /opt
+git clone https://github.com/golismero/golismero.git
+cd golismero
+pip install -r requirements.txt
+pip install -r requirements_unix.txt
+ln -s ${PWD}/golismero.py /usr/bin/golismero
+cd ..
+
+apt install xsser dmitry wapiti uniscan nikto dnsmap dnswalk -y
